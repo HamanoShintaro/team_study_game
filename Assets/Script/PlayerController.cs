106a6d478d75abb7e3a8b7eb5470c92f913f871f
ShintaroHamano <shintaro.action@gmail.com>
2020-06-06 13:35:23 +0900

    2020/0606 ペンギンの歩くモーションと、ジャンプ時にジャンプした画像に切り替わる処理の追加

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -17,8 +17,12 @@ namespace touch_game
         public float gravity;
         public float jump;
         private float timeRunningMotion;
-        public Sprite penguinR;
-        public Sprite penguinL;
+
+        // 進化段階1用のSpriteの定義
+        public Sprite running00, running01, running02, running03, running04,
+             running05, running06, running07, running08;
+        public Sprite jump00;
+        private Dictionary<int, Sprite> running;
 
 
         // Start is called before the first frame update
@@ -28,7 +32,22 @@ namespace touch_game
             initPosition = new Vector3(30.0f, 30.0f, 0.0f);
             playerImage.transform.position = initPosition;
             velocity = 0.0f;
+            InitPlayer();
         }
+
+        void InitPlayer()
+        {
+            //モーション用に連想配列へSpriteをセット
+            running = new Dictionary<int, Sprite>{
+                {0, running00}, {1, running01}, {2, running02}, {3, running03},
+                {4, running04}, {5, running03}, {6, running02}, {7, running01},
+                {8, running00}, {9, running05}, {10, running06}, {11, running07},
+                {12, running08}, {13, running07}, {14, running06}, {15, running05}
+            };
+            this.StartCoroutine(this.runnningMotion());
+        }
+
+
 
         // Update is called once per frame
         void Update()
@@ -36,29 +55,20 @@ namespace touch_game
 
             if (playerImage.transform.position.y <= initPosition.y)
             {
-                
+
 
                 if (velocity < 0.0f)
                 {
                     velocity = 0.0f;
                 }
-
-                if(timeRunningMotion > 0.3f)
-                {
-                    timeRunningMotion = 0.0f;
-                    if (playerImage.sprite == penguinL) { playerImage.sprite = penguinR; } else { playerImage.sprite = penguinL; }
-                    GameController.instance.seController.playSeRunning(1);
-                }else
-                {
-                    timeRunningMotion += Time.deltaTime;
-                }
-
+                
                 if (Input.GetKeyDown(KeyCode.Space))
                 {
                     Debug.Log("ジャンプします");
                     velocity = jump;
                     GameController.instance.voiceController.playVoiceJump(1);
                     GameController.instance.seController.playSeJump(1);
+                    playerImage.sprite = jump00;
                     this.StartCoroutine(this.randingEvent());
                 }
             }
@@ -70,6 +80,7 @@ namespace touch_game
             }
             playerImage.transform.position = new Vector3(playerImage.transform.position.x, playerImage.transform.position.y + velocity * Time.deltaTime, 0.0f);
         }
+
         public IEnumerator randingEvent()
         {
             yield return new WaitForSeconds(0.1f);
@@ -78,6 +89,25 @@ namespace touch_game
                 yield return new WaitForSeconds(0.02f);
             }
             GameController.instance.seController.playSeLanding(1);
+        }
+
+        public IEnumerator runnningMotion()
+        {
+            int runLevel = 0;
+
+            while (true) {
+                while (playerImage.transform.position.y <= initPosition.y)
+                {
+                    playerImage.sprite = running[runLevel];
+                    if (runLevel == 15) { runLevel = 0; } else { runLevel += 1; }
+                    if (runLevel == 7 || runLevel == 15) {
+                        GameController.instance.seController.playSeRunning(1);
+                    }
+                    yield return new WaitForSeconds(0.05f);
+                }
+                runLevel = 0;
+                yield return new WaitForSeconds(0.05f);
+            }
         }
 
     }
