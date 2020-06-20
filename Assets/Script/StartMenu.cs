01391c3f8b433c39f9a0da2be65b325595841caf
LAPTOP-6VOU7IKR\nobuhiro <namatame@joyworks.jp>
2020-06-20 12:49:24 +0900

    MainStage、StartManu関連の整理

--- Assets/Script/StartMenu.cs
+++ Assets/Script/StartMenu.cs
@@ -21,29 +21,10 @@ namespace touch_game {
         // Update is called once per frame
         void Update()
         {
-            if (displayFlg == true)
-            {
-                if(startMenu.alpha < 1.0f)
-                { 
-                    startMenu.alpha = alpha;
-                    alpha += speed;
-                }
-                if(startMenu.interactable == false)
-                {
-                    startMenu.interactable = true;
-                }
-            }
-            if (displayFlg == false)
-            {
-                if (startMenu.alpha > 0f)
-                {
-                    startMenu.alpha = 0;
-                    alpha = 0f;
-                }
-                if (startMenu.interactable == true)
-                {
-                    startMenu.interactable = false;
-                }
+            if(startMenu.alpha < 1.0f)
+            { 
+                startMenu.alpha = alpha;
+                alpha += speed;
             }
         }
     }
