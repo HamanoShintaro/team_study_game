413faff280ee8dae61059b5a29315a3e37d52179
LAPTOP-6VOU7IKR\nobuhiro <namatame@joyworks.jp>
2020-06-27 10:53:10 +0900

    Merge branch 'develop' of https://github.com/HamanoShintaro/team_study_game into develop

---
+++ Assets/Script/RandomStage.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomStage : MonoBehaviour
+{
+    public int minHeight;
+    public int maxHeight;
+    public float blockPosition;
+    public GameObject block;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ChangeHeight();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ChangeHeight()
+    {
+        //ランダムな高さを生成して設定
+        int height = Random.Range(minHeight, maxHeight);
+        block.transform.localPosition = new Vector3(blockPosition, height, 0.0f);
+        Debug.Log(height);
+    }
+}
