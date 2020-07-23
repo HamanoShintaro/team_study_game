46ac9213524198e6e9e9dda045b2e5024ae3411e
LAPTOP-6VOU7IKR\nobuhiro <namatame@joyworks.jp>
2020-07-24 01:17:04 +0900

    記録画面にいろいろ追加。画面遷移っぽいところまで実装。

---
+++ Assets/Script/RecodePageController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace touch_game
+{
+    public class RecodePageController : MonoBehaviour
+    {
+        /// <summary>
+        /// プレイ記録画面オブジェクト
+        /// </summary>
+        public GameObject Recode;
+
+        /// <summary>
+        /// プレイヤーペンギンの進化到達段階の記録画面オブジェクト
+        /// </summary>
+        public GameObject PenginEvolution;
+
+        /// <summary>
+        /// ゲームプレイ中に見たエピソード表示画面オブジェクト
+        /// </summary>
+        public GameObject Episode;
+
+        /// <summary>
+        /// 記録画面の初期表示設定を行う。
+        /// </summary>
+        private void OnEnable()
+        {
+            this.Recode.SetActive(true);
+            this.PenginEvolution.SetActive(false);
+            this.Episode.SetActive(false);
+        }
+
+        /// <summary>
+        /// 記録画面の切り替えを行う。
+        /// </summary>
+        /// <remarks></remarks>
+        /// <param name="viewObject">
+        /// 表示する記録画面のオブジェクト。プレイ記録画面、ペンギン進化記録画面、エピソード記録画面のいずれか。
+        /// </param>
+        public void PegeChange(GameObject viewObject)
+        {
+            //全ての記録画面を非表示にする
+            this.Recode.SetActive(false);
+            this.PenginEvolution.SetActive(false);
+            this.Episode.SetActive(false);
+
+            //引数の記録画面オブジェクトのみを表示する
+            viewObject.SetActive(true);
+        }
+    }
+}
