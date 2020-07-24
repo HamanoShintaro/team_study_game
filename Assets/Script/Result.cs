981a50834b96e65fbcf0bab3acb53823a59613d1
shinagawa_joy <shinagawa@joyworks.jp>
2020-07-24 12:33:24 +0900

    リザルトのバックグラウンド分岐と新記録画像の制御

--- Assets/Script/Result.cs
+++ Assets/Script/Result.cs
@@ -6,11 +6,52 @@ using UnityEngine.UI;
 public class Result : MonoBehaviour
 {
     public GameObject MileageNewestRecord;
+    public GameObject NewRecordImage;
+    public Image ResultBackground;
+    public Sprite image1;
+    public Sprite image2;
+    public Sprite image3;
+    public Sprite image4;
+    public int Evo1;
+    public int Evo2;
+    public int Evo3;
     private int NewestRecord;
 
     void OnEnable()
     {
+        NewRecordImage.SetActive(false);
         NewestRecord = PlayerPrefs.GetInt("MileageNewestRecord", 0);
+
+        if (Evo1 <= NewestRecord)
+        {
+            ResultBackground = this.GetComponent<Image>();
+            ResultBackground.sprite = image1;
+        }
+
+        else if (Evo2 <= NewestRecord)
+        {
+            ResultBackground = this.GetComponent<Image>();
+            ResultBackground.sprite = image2;
+        }
+
+        else if (Evo3 <= NewestRecord)
+        {
+            ResultBackground = this.GetComponent<Image>();
+            ResultBackground.sprite = image3;
+        }
+
+        else
+        {
+            ResultBackground = this.GetComponent<Image>();
+            ResultBackground.sprite = image4;
+        }
+
+        if (PlayerPrefs.GetInt("MileageHighestRecord", 0) < NewestRecord)
+        {
+            NewRecordImage.SetActive(true);
+        }
+
+
     }
 
     // Update is called once per frame
